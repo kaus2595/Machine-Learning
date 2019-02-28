52b58b9104fbcbf46b1d8acff4cf49b8d969ef90
Kaustubh <kausr2595@gmail.com>
2019-02-28 13:31:38 +0530

    SkLearn

--- Linear Regression and Decision Tree/Linear_Regression_Sklearn.py
+++ Linear Regression and Decision Tree/Linear_Regression_Sklearn.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("california_housing_train.csv")
 
@@ -12,7 +13,13 @@ x = x.reshape(n,1)
 reg = LinearRegression()
 reg = reg.fit(x,y)
 
+plt.plot(x,y,color='#58b970', label="Regression Line")
+plt.scatter(x,y, c="#ef5423", label = "Scatter Plot")
+plt.xlabel("Population")
+plt.ylabel("Median House Value")
+plt.legend()
 y_pred = reg.predict(x)
 
 r2_score = reg.score(x,y)
 print(r2_score)
+plt.show()
